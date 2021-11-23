91c2b54378ac1d0ea30a8e072dc5bcd4d22afe9b
salvador salazar <salvador.g.salazar79@gmail.com>
2021-11-22 23:04:08 -0600

    sort-food update

--- src/main/java/com/example/intellifridge/repositories/FridgeRepository.java
+++ src/main/java/com/example/intellifridge/repositories/FridgeRepository.java
@@ -4,9 +4,26 @@ import com.example.intellifridge.models.Food;
 import com.example.intellifridge.models.Fridge;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface FridgeRepository extends JpaRepository <Fridge, Long>{
+
     List<Food> findByNameContaining(String title, Sort sort);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM foods where id = ? Order By name asc ")
+    List<Food> findAllByNameAsc(String id);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM foods where id = ? Order By name desc ")
+    List<Food> findAllByNameDesc(String name);
+
+//    @Query(nativeQuery = true,value = "SELECT * FROM foods Order By is_in_Freezer asc ")
+//    List<Food> findAllByNameAsc(String name);
+
+
+
+
+
+
 }
