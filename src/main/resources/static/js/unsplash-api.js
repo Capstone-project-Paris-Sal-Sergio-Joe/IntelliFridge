cdf793c863f9e6a9a7df29aa5f46f6bb7b8649cb
Joseph Congdon <jocongdon@yahoo.com>
2021-11-29 18:50:21 -0600

    started the unsplash api

---
+++ src/main/resources/static/js/unsplash-api.js
@@ -0,0 +1,23 @@
+
+fetch('https://api.unsplash.com/search/photos?client_id=' + "0vWQD0zIliLfVCQmhxLcCFmdUg12C4A0-0JUgmc4qNg&query=cats")
+    .then(res => res.json())
+    .then(console.log)
+    .catch(console.log)
+
+
+function renderAddFoodImage(object){
+    var html = '<div class="add-food-image">';
+    html += `<img src="${object.results.url.small}" />`;
+    html += `<small> +${object.results.user.name} </small>`;
+    html += '</div>';
+
+    return html;
+}
+
+let imageSelector = document.getElementById("foodName");
+
+$("foodName").keyup(function (){
+    const selectedImage = imageSelector.value;
+
+
+})
