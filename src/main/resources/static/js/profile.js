0503cd139dc2c98bec5376a790fc60aa99faa133
Joseph Congdon <jocongdon@yahoo.com>
2021-12-01 01:18:41 -0600

    started the user delete process

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -4,3 +4,12 @@ const body = $('body');
 body.on("click", "#edit", function (){
     $(`#editForm`).toggleClass("hidden")
 });
+
+$(`#unsubscribe`).click(function(){
+    if(window.confirm('are you sure you want to unsubscribe?'))
+    {
+        console.log("they clicked yes")
+    } else{
+        console.log("they clicked no")
+    }
+});
