f306741dfbf3b45be91f1a7880b78096ffccc7ab
SerguioL <serguio.l.landa@gmail.com>
2021-11-30 22:42:50 -0600

    almost done just need to add fridge name to message

--- src/main/java/com/example/intellifridge/models/Twilio.java
+++ src/main/java/com/example/intellifridge/models/Twilio.java
@@ -5,6 +5,11 @@ import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class Twilio {
 
@@ -17,22 +22,57 @@ public class Twilio {
     @Value("${PHONE_NUMBER}")
     private String PHONE_NUMBER;
 
-    public void twilioTest(){
+//    public void twilioTest(){
+//        com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//
+//        Message message = Message.creator(new PhoneNumber("userPhoneNumber"),//send to
+//                new PhoneNumber(PHONE_NUMBER),//send from
+//                "hello").create();
+//
+//        System.out.println(message.getSid());
+//    }
+
+//    public void sendNotification(List<Food> foods, User user){
+//        com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+//
+//        for(int i=0; i<foods.size(); i++){
+//            foods.get(i).getName();
+//        }
+//
+//        SimpleDateFormat foodDate = new SimpleDateFormat("MM/dd/yyyy");
+//        String formattedDate = foodDate.format(food.getExpirationDate());
+//
+//        Message message = Message.creator(new PhoneNumber(user.getPhoneNumber()),//send to
+//                new PhoneNumber(PHONE_NUMBER),//send from
+//                "Your " + food.getName() + " is going to expire on " + formattedDate + " its from your " + food.getFridge().getName() + " fridge.").create();
+//
+//        System.out.println(message.getSid());
+//    }
+
+    public void sendNotification(List<String> foods,User user){//,int fridgeId
         com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Message message = Message.creator(new PhoneNumber("userPhoneNumber"),//send to
-                new PhoneNumber(PHONE_NUMBER),//send from
-                "hello").create();
 
-        System.out.println(message.getSid());
-    }
+//        for(int i=0; i<foods.size(); i++){
+////            System.out.print(foods.get(i).getName() + " " + foods.get(i).getExpirationDate() + ", ");
+//            output = " " + foods.get(i).getName() + " " + foods.get(i).getExpirationDate() + ", ";
+//
+//        }
 
-    public void sendNotification(Food food, User user){
-        com.twilio.Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        StringBuilder strbul=new StringBuilder();
+        for(String str : foods)
+        {
+            strbul.append(str);
+            //for adding comma between elements
+            strbul.append(",");
+        }
+        String str=strbul.toString();
+
+        System.out.println(str);
 
         Message message = Message.creator(new PhoneNumber(user.getPhoneNumber()),//send to
                 new PhoneNumber(PHONE_NUMBER),//send from
-                "your" + food.getName() + "is going to expire soon").create();
+                "Hello " + user.getUsername() + " these foods are going to expire soon " + str + " there in your ").create();//+ user.getFridges().get(fridgeId).getName()
 
         System.out.println(message.getSid());
     }
