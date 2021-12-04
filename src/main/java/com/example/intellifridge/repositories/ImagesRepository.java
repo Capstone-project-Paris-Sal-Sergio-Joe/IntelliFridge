c19baf507eb92c681a25626e264b0c39ccc8c892
SalvadorSalazar <86627387+salvadorsalazar@users.noreply.github.com>
2021-12-04 03:55:01 -0600

    Delete ImagesRepository.java

--- src/main/java/com/example/intellifridge/repositories/ImagesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.intellifridge.repositories;
-
-import com.example.intellifridge.models.FileImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImagesRepository extends JpaRepository <FileImage, Long>{
-
-}
