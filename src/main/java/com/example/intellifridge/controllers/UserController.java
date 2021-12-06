de83d185e1e4526445d3a80df227c7bd2d2543d8
salvador salazar <salvador.g.salazar79@gmail.com>
2021-12-06 08:58:55 -0600

    login . signup view updates test

--- src/main/java/com/example/intellifridge/controllers/UserController.java
+++ src/main/java/com/example/intellifridge/controllers/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
     @GetMapping("/sign-up")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
-        return "users/sign-up";
+        return "users/sign-up2";
     }
 
     @PostMapping("/sign-up")
