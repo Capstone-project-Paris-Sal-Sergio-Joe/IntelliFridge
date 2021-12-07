cdd7b72be56e0033c15e45dc03c75adaa234b3d2
SerguioL <serguio.l.landa@gmail.com>
2021-12-07 15:54:24 -0600

    almost done

---
+++ src/main/resources/static/js/frontendValidation.js
@@ -0,0 +1,51 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const signUpForm = document.getElementById('signUpForm');
+const errorSignUp = document.getElementById('errorSignUp');
+const validatedEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const editForm = document.getElementById('editForm');
+const phoneNumber = document.getElementById('phoneNumber');
+const validatedPhoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+
+signUpForm.addEventListener('submit',(e) =>{
+    let messages = [];
+
+
+    if(email.value.match(validatedEmail)){
+
+    } else {
+        messages.push('This is not a valid email');
+    }
+
+    if(password.value.length <= 8){
+        messages.push('Password must be longer than 6 characters');
+    }
+
+    if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters')
+    }
+
+    if(messages.length >0){
+        e.preventDefault()
+        errorSignUp.innerText = messages.join(', ');
+    }
+})
+
+editForm.addEventListener('submit', (e) => {
+    let messages = [];
+
+    if(email.value.match(validatedEmail)){
+
+    } else {
+        messages.push('This is not a valid email');
+    }
+
+    if(phoneNumber.value.length )
+
+    if(messages.length >0){
+        e.preventDefault()
+        errorSignUp.innerText = messages.join(', ');
+    }
+})
