430ed44437fe71d7d80197bea6c7703b1a63055c
Paris Shirley <shirleyp1097@gmail.com>
2021-12-11 16:34:53 -0600

    views look good. still need search bar and sort by for mobile view

--- src/main/resources/static/js/fridge.js
+++ src/main/resources/static/js/fridge.js
@@ -5,12 +5,24 @@ $(document).ready(function () {
         "info": false
     });
 
-    // add food image after name in table
+    // add food image after name in desktop view table
     $('.food-name').each(async function () {
             let fridgeImage = $(this).text()
             let fridgeImageUrl = await imageCode(fridgeImage);
-                $(this).append(`
+                $(this).prepend(`
                 <img src="${fridgeImageUrl}" class="foodResultImage img-thumbnail">
+                <br>
+            `)
+        }
+    )
+
+    // add food image after name in mobile view table
+    $('.foodNameMobile').each(async function () {
+            let fridgeImage = $(this).text()
+            let fridgeImageUrl = await imageCode(fridgeImage);
+            $(this).prepend(`
+                <img src="${fridgeImageUrl}" class="foodResultImage img-thumbnail m-2">
+                <br>
             `)
         }
     )
