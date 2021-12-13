f1211d982bf209500fedd205187a6240e05326b6
SerguioL <serguio.l.landa@gmail.com>
2021-12-13 10:48:55 -0600

    fix frontend validation on sign up

--- src/main/resources/static/js/frontendValidation.js
+++ src/main/resources/static/js/frontendValidation.js
@@ -1,25 +1,13 @@
 const username = document.getElementById('username');
-const email = document.getElementById('email');
 const password = document.getElementById('password');
 const passwordConfirm = document.getElementById('confirm');
 const signUpForm = document.getElementById('signUpForm');
 const errorSignUp = document.getElementById('errorSignUp');
-const validatedEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
-const phoneNumber = document.getElementById('phoneNumber');
-const validatedPhoneNumber = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 const backendError = document.getElementById('errorM');
-
 
 signUpForm.addEventListener('submit',(e) =>{
 
     let messages = [];
-
-
-    if(email.value.match(validatedEmail)){
-
-    } else {
-        messages.push('This is not a valid email');
-    }
 
     if(password.value.length <= 8){
         messages.push('Password must be longer than 8 characters');
@@ -31,16 +19,6 @@ signUpForm.addEventListener('submit',(e) =>{
 
     if (password.value !== passwordConfirm.value) {
         messages.push('Password and password confirmation are not the same');
-    }
-
-    if(phoneNumber.value.match(validatedPhoneNumber)){
-
-    } else if (phoneNumber.value === '') {
-
-    } else if (phoneNumber.value === null) {
-
-    } else {
-        messages.push('This is not a valid phone number');
     }
 
     if(messages.length >0){
